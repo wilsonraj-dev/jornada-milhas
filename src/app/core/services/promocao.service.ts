d1d40d75c5d742fdf8db27413fe4c29105b2f097
Wilson Junior <wilson.assis.junior@gmail.com>
2025-07-27 17:55:43 -0300

    Consumindo API

--- src/app/core/services/promocao.service.ts
+++ src/app/core/services/promocao.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Promocao } from '../types/types';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromocaoService {
 
-  constructor() { }
+  private apiUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Promocao> {
+    return this.http.get<Promocao>(`http://localhost:8080/promocoes`);
+  }
 }
