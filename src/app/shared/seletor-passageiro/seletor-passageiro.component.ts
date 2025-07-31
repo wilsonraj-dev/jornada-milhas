2c78a3443365f533d05b8e32f34ecbb40bdc954d
Wilson Junior <wilson.assis.junior@gmail.com>
2025-07-31 20:54:07 -0300

    Componente de passageiro

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-seletor-passageiro',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./seletor-passageiro.component.scss']
 })
 export class SeletorPassageiroComponent {
-
+  @Input() titulo: string = '';
+  @Input() subtitulo: string = '';
 }
