604961a0ad564f2acfd44db1e6c1df84ca2a0ac4
Wilson Junior <wilson.assis.junior@gmail.com>
2025-07-31 20:15:21 -0300

    Abertura e fechamento modal

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ export class FormBuscaService {
 
   formBusca: FormGroup;
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
     });
@@ -20,5 +22,11 @@ export class FormBuscaService {
       throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
     return control as FormControl;
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    });
   }
 }
