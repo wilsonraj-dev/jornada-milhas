a10631b9fd9d0bbce09e69cda9a0217596c36e77
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-04 20:37:58 -0300

    Regra para incrementar e decrementar botao de passageiros

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,11 +1,47 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-seletor-passageiro',
   templateUrl: './seletor-passageiro.component.html',
   styleUrls: ['./seletor-passageiro.component.scss']
 })
-export class SeletorPassageiroComponent {
+export class SeletorPassageiroComponent implements ControlValueAccessor {
+
   @Input() titulo: string = '';
   @Input() subtitulo: string = '';
+
+  value: number = 0;
+  onChange = (val: number) => { };
+  onTouch = () => { };
+
+  writeValue(val: any): void {
+    this.value = val;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+  incrementar() {
+    this.value += 1;
+    this.onChange(this.value);
+    this.onTouch();
+  }
+
+  decrementar() {
+    if (this.value > 0) {
+      this.value -= 1;
+      this.onChange(this.value);
+      this.onTouch();
+    }
+  }
 }
