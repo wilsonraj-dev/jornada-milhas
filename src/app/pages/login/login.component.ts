89e45d947952722449429d15165aacad0cc6c9c1
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-05 20:50:57 -0300

    Serviço de autenticação

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,
+    private authService: AutenticacaoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null],
+      senha: [null]
+    })
+  }
+
+  login() {
+    const email = this.loginForm.value.email;
+    const senha = this.loginForm.value.senha;
+
+    this.authService.autenticar(email, senha).subscribe({
+      next: (value) => {
+        console.log('Login successful', value);
+        this.router.navigateByUrl('/');
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      }
+    });
+  }
 }
