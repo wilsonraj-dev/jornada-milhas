ab86d1e52c6ab58246c074a2a367874ad9d45d43
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-12 21:17:18 -0300

    Métodos http GET e PATCH

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { PessoaUsuaria } from '../types/types';
@@ -13,5 +13,22 @@ export class CadastroService {
 
   cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria> {
     return this.http.post<PessoaUsuaria>(`http://localhost:8080/auth/cadastro`, pessoaUsuaria);
+  }
+
+  buscarCadastro(token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get<PessoaUsuaria>(`http://localhost:8080/auth/perfil`, { headers });
+  }
+
+  editarCadastro(pessoaUsuaria: PessoaUsuaria, token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.patch<PessoaUsuaria>(`http://localhost:8080/auth/perfil`,
+      pessoaUsuaria, { headers });
   }
 }
