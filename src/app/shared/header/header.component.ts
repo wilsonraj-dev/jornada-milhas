44e637ae25c31afa5e4dcc24e282af91f1227764
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-19 20:39:28 -0300

    Ajustando o header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private userService: UserService,
+    private router: Router
+  ) { }
 
+  user$ = this.userService.retornarUser();
+  
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
