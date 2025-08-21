45b6f8efd91113241923555e2b433379404250ba
Wilson Junior <wilson.assis.junior@gmail.com>
2025-08-21 20:36:24 -0300

    Att

--- src/app/core/services/passagens.service.ts
+++ src/app/core/services/passagens.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Resultado } from '../types/types';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class PassagensService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getPassagens(search: any) {
+  getPassagens(search: any): Observable<Resultado> {
     const params = search;
     return this.httpClient.get<Resultado>(this.apiUrl + '/passagens/search', { params });
   }
